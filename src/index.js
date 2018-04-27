5d737a70925b53ad4752c0440be0526902ea1e35
Semenov Evgeniy <semenov@tutu.ru>
2018-04-27 22:12:27 +0300

    Add exports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './global.css'
 
 export { default as Text } from './Text';
+export { default as Panel } from './Panel';
 export { default as Button } from './Button';
 export { default as ScrollTop } from './ScrollTop';
+export { default as HorizontalScroll } from './HorizontalScroll';
 
 import * as utils from './utils';
 export { utils };
