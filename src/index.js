fc72e27aae1877475a646e8499de8ee27c9773f8
Semenov Evgeniy <semenov@tutu.ru>
2018-04-28 22:59:10 +0300

    Fix exports

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export { default as Text } from './Text';
 export { default as Panel } from './Panel';
 export { default as Button } from './Button';
 export { default as ScrollTop } from './ScrollTop';
+export { default as SearchBar } from './SearchBar';
 export { default as HorizontalScroll } from './HorizontalScroll';
 
 import * as utils from './utils';
